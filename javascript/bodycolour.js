046529f54ac2cc35e0a0e1ed3ea8119180901841
shimul330 <shimuldevnath330@gmail.com>
2025-03-02 19:32:02 +0600

    added a funtional file

--- javascript/bodycolour.js
+++ javascript/bodycolour.js
@@ -1,10 +1,10 @@
 
-const colours = ["red", "blue", "yellow", "purple"];
+const colours = ["red", "blue", "yellow", "purple", "orange", "amber"];
 
 
 document.getElementById("body-change-colour").addEventListener("click", function(){
     const randomColour = Math.floor(Math.random() * colours.length);
-    
+
     document.body.style.backgroundColor = colours[randomColour];
     document.body.style.transition = "0.3s";
     
